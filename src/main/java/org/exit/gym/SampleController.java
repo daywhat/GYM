cfb1d0fc2ce4acced849cce3477fab18c3966d65
daywhat <daywhat@hotmail.com>
2018-08-14 14:24:15 +0900

    version 1.0.7
    next SampleVO

--- src/main/java/org/exit/gym/SampleController.java
+++ src/main/java/org/exit/gym/SampleController.java
@@ -1,5 +1,11 @@
 package org.exit.gym;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.exit.model.dto.SampleVO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,44 @@ public class SampleController {
 		
 		return "Hello World";
 	}
+	
+	
+	@RequestMapping("/sendVO")
+	public SampleVO sendVO() {
+		SampleVO vo = new SampleVO();
+		
+		vo.setFirstName("�浿");
+		vo.setLastName("ȫ");
+		vo.setMno(123);
+		
+		return vo;
+		
+	}
+	
+	
+	@RequestMapping("/sendList")
+	public List<SampleVO>sendList(){
+		
+		List<SampleVO> list = new ArrayList<>();
+		for(int i = 0; i < 10; i++) {
+			SampleVO vo = new SampleVO();
+			vo.setFirstName("�浿");
+			vo.setLastName("ȫ");
+			vo.setMno(i);
+			list.add(vo);
+		}
+		
+		return list;
+		
+	}
+	
+	@RequestMapping("/sendMap")
+	public Map<Integer, SampleVO> sendMap(){
+		
+		Map<Integer, SampleVO> Map = new HashMap<>();
+		
+		return null;
+	}
+	
+	
 }
---
+++ src/main/java/org/exit/model/dto/SampleVO.java
@@ -0,0 +1,27 @@
+package org.exit.model.dto;
+
+public class SampleVO {
+
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	public int getMno() {
+		return Mno;
+	}
+	public void setMno(int mno) {
+		Mno = mno;
+	}
+	private String FirstName;
+	private String LastName;
+	private int Mno;
+	
+}
